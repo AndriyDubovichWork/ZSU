20a21a5532e9a2ee1a3d9f1858a95cd50ea64f35
AndriyDubovichWork <andriydubovichwork@gmail.com>
2022-04-17 23:58:54 +0300

    created app

---
+++ src/Components/App/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Lending from '../Lending/Lending';
+//@ts-ignore
+import style from './App.module.scss';
+
+function App() {
+  return (
+    <>
+      <Lending />
+    </>
+  );
+}
+
+export default App;
